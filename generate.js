7df8a2a48f7ec3907a17bdf4b9c377af87ec88d0
jabedakhtar785-sudo <jabedakhtar785@gmail.com>
2026-02-23 14:46:29 +0530

    Add files via upload

--- generate.js
+++ generate.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
     });
     
     const data = await response.json();
-    res.status(200).json({ text: data.choices[0].message.content });
+    res.status(200).json({ text: data.choices?.[0]?.message?.content || "No response" });
     
   } catch (err) {
     res.status(500).json({ error: err.message });
