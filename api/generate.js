4d89448c8c542997d9f03be1acefd4e492e9e2ce
jabedakhtar785-sudo <jabedakhtar785@gmail.com>
2026-02-23 16:02:24 +0530

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -28,6 +28,6 @@ export default async function handler(req, res) {
     });
 
   } catch (err) {
-    res.status(500).json({ error: "Server error" });
+    res.status(500).json({ error: err.message });
   }
 }
